c0209913731158dbcc427f20f819668e1eaddf70
David Newkerk <keysplayer@gmail.com>
2014-11-29 21:42:33 -0800

    Add Bootstrap for layout/style, add helper method for Bootstrap messages, update gems, install jquery-ui.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  # Bootstrap CSS classes for flash messages.
+  def bootstrap_class_for(flash_type)
+    case flash_type
+      when :notice
+        "alert-info"
+      when :success
+        "alert-success"
+      when :alert
+        "alert-warning"
+      when :error
+        "alert-danger"
+      else
+        "alert-info " + flash_type.to_s
+    end
+  end
+
 end
