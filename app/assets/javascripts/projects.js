005207b645d4adfceb02ec1fadd2eae453102aa3
David Newkerk <keysplayer@gmail.com>
2014-12-02 15:30:49 -0800

    Update hidden position fields after each sort operation.

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,35 +1,27 @@
 ready = function() {
 
   // Make the form sortable
-  $("form.sortable ul").sortable({
+  $(".sortable").sortable({
     axis:   "y",
     handle: ".handle",
     placeholder: "placeholder",
-    update: function() {
-      console.log("Sorted");
-    }
-  });
-
-  // On submit, set the value of the position inputs to the value of their current physical position in the list
-  $("form.sortable").submit(function() {
-    $("form.sortable ul li input.position").each(function(index) {
-      $(this).val(index);
-    });
-
-    return true;
+    update: updatePositions
   });
 
 
   // Bind the + behavior
-  $(".add a").click(function() {
-    var num = $("form.sortable ul").children().length + 1;
+  $(".add a").click(function(event) {
+    event.preventDefault();
+    var num = $(".sortable").children().length + 1;
     var newInput = getNewInputHtml(num);
 
-    $("form.sortable ul").append(newInput);
+    $(".sortable").append(newInput);
 
     // Bind the X behavior to the new input element
     bindIconCloseEvent();
-    return false;
+
+    // Update positions to account for the new item
+    updatePositions();
   });
 
   // Bind the X behavior to the original elements
@@ -37,11 +29,22 @@ ready = function() {
 };
 
 
-function bindIconCloseEvent() {
+// After sorting, set the value of the position inputs to the value of their
+// current physical position in the list.
+function updatePositions(event) {
+  console.log("called");
+  var inputs = $(event.target).find("input.position");
+  inputs.each(function(index) {
+    $(this).val(index + 1);
+  });
+}
+
+
+function bindIconCloseEvent(event) {
   $(".remove").click(function() {
+    event.preventDefault();
     $(this).parents("li").find("input.destroy").val(1);
     $(this).parents("li").hide("slow");
-    return false;
   });
 }
 
@@ -51,12 +54,12 @@ function getNewInputHtml(num) {
   var newInput = '' +
     '<li>' +
       '<div class="row">' +
-        '<div class="col-md-11">' +
+        '<div class="col-md-1">' +
+          '<i class="handle fa fa-arrows"></i>' +
+        '</div>' +
+        '<div class="col-md-10">' +
           '<div class="form-group">' +
-            '<div class="input-group">' +
-              '<span class="handle input-group-addon"><i class="fa fa-arrows"></i></span>' +
-              '<input class="name form-control" name="project[todos_attributes][' + num + '][name]" size="30" type="text" value="" />' +
-            '</div>' +
+            '<input class="name form-control" name="project[todos_attributes][' + num + '][name]" size="30" type="text" value="" />' +
             '<input class="position" name="project[todos_attributes][' + num + '][position]" type="hidden" value="" />' +
             '<input class="destroy" name="project[todos_attributes][' + num + '][_destroy]" type="hidden" value="false">' +
           '</div>' +
